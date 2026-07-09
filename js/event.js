cd250961dbadf64c1a99c768f846d06382e7275d
YokoVNHub <ziney7612@gmail.com>
2026-07-09 23:21:08 +0700

    Update fetch URL to gallery.json
    Changed the fetch URL from events.json to gallery.json.

--- js/event.js
+++ js/event.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     try {
 
-        const response = await fetch("../data/events.json");
+        const response = await fetch("../data/gallery.json");
 
         if (!response.ok) {
 
